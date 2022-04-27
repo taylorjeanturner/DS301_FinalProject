c307eeee0639899872ec94b75740e73caf55ac5d
Taylor Turner <taylorjeanturner@gmail.com>
2022-04-27 13:48:54 -0500

    more logisitic regression

--- TaylorTurner_logreg.R
+++ TaylorTurner_logreg.R
@@ -1,12 +1,15 @@
+library(MASS)
+library(dplyr)
+library(ROCR)
 
-df <- readr::read_csv('./DS301_FinalProject/heart_2020_cleaned.csv')
-df$HeartDisease <- ifelse(df$HeartDisease == 'Yes', 1, 0)
+df = readr::read_csv('./DS301_FinalProject/heart_2020_cleaned.csv')
+df$HeartDisease = ifelse(df$HeartDisease == 'Yes', 1, 0)
 df$Smoking = factor(df$Smoking)
 df$AlcoholDrinking = factor(df$AlcoholDrinking)
 df$Stroke = factor(df$Stroke)
 df$DiffWalking = factor(df$DiffWalking)
 df$Sex = factor(df$Sex)
-df$AgeCategory = factor(df$AgeCategory)
+#df$AgeCategory = factor(df$AgeCategory)
 df$Race = factor(df$Race)
 df$Diabetic = factor(df$Diabetic)
 df$PhysicalActivity = factor(df$PhysicalActivity)
@@ -15,14 +18,52 @@ df$Asthma = factor(df$Asthma)
 df$KidneyDisease = factor(df$KidneyDisease)
 df$SkinCancer = factor(df$SkinCancer)
 
+df$AgeCategory = ifelse(df$AgeCategory == '18-24', 21,
+                        ifelse(df$AgeCategory == '25-29', 27,
+                               ifelse(df$AgeCategory == '30-34', 32,
+                                      ifelse(df$AgeCategory == '35-39', 37,
+                                             ifelse(df$AgeCategory == '40-44', 42,
+                                                    ifelse(df$AgeCategory == '45-49', 47,
+                                                           ifelse(df$AgeCategory == '50-54', 52,
+                                                                  ifelse(df$AgeCategory == '55-59', 57,
+                                                                         ifelse(df$AgeCategory == '60-64', 62,
+                                                                                ifelse(df$AgeCategory == '65-69', 67,
+                                                                                       ifelse(df$AgeCategory == '70-74', 72,
+                                                                                              ifelse(df$AgeCategory == '75-79', 77,
+                                                                                                     ifelse(df$AgeCategory == '80 or older', 80, 0)))))))))))))
+
 set.seed(100)
 n = nrow(df)
 train = sample(1:nrow(df),nrow(df)/2, replace=FALSE)
 test = (-train)
 
+# full model
 glm.fit = glm(HeartDisease~., data=df, subset=train, family='binomial')
 glm.prob = predict(glm.fit, df[test,], type='response') 
-glm.pred = rep(0,length(test)+1)
+glm.pred = rep(0,dim(df[test,])[1])
 glm.pred[glm.prob > 0.5] = 1
 table(glm.pred, df[test,]$HeartDisease)
 1-mean(glm.pred == df[test,]$HeartDisease)
+
+ROCRpred <- prediction(glm.prob,df[test,]$HeartDisease)
+plot(performance(ROCRpred,'tnr','fnr'),colorize=TRUE,
+     print.cutoffs.at=seq(0,1,by=0.05), text.adj=c(-0.2,1.7))
+auc_ROCR <- performance(ROCRpred, measure = "auc")
+auc_ROCR@y.values[[1]] 
+12199 / (12199 + 1479)
+1239 / (1239+144981)
+
+glm.prob = predict(glm.fit, df[test,], type='response') 
+glm.pred = rep(0,dim(df[test,])[1])
+glm.pred[glm.prob > 0.1] = 1
+table(glm.pred, df[test,]$HeartDisease)
+1-mean(glm.pred == df[test,]$HeartDisease)
+3451 / (3451 + 10227)
+32165 / (32165+114055)
+
+
+library(corrplot)
+library(caret)
+dm = data.matrix(df[,names(df) != 'HeartDisease'])
+cor(dm)
+                 
