dbfde1677d93160e4343d409c57cf484561b5986
Taylor Turner <taylorjeanturner@gmail.com>
2022-05-03 10:53:31 -0500

    updated code

--- TaylorTurner_logreg.R
+++ TaylorTurner_logreg.R
@@ -9,7 +9,6 @@ df$AlcoholDrinking = factor(df$AlcoholDrinking)
 df$Stroke = factor(df$Stroke)
 df$DiffWalking = factor(df$DiffWalking)
 df$Sex = factor(df$Sex)
-#df$AgeCategory = factor(df$AgeCategory)
 df$Race = factor(df$Race)
 df$Diabetic = factor(df$Diabetic)
 df$PhysicalActivity = factor(df$PhysicalActivity)
@@ -18,20 +17,21 @@ df$Asthma = factor(df$Asthma)
 df$KidneyDisease = factor(df$KidneyDisease)
 df$SkinCancer = factor(df$SkinCancer)
 
-df$AgeCategory = ifelse(df$AgeCategory == '18-24', 21,
-                        ifelse(df$AgeCategory == '25-29', 27,
-                               ifelse(df$AgeCategory == '30-34', 32,
-                                      ifelse(df$AgeCategory == '35-39', 37,
-                                             ifelse(df$AgeCategory == '40-44', 42,
-                                                    ifelse(df$AgeCategory == '45-49', 47,
-                                                           ifelse(df$AgeCategory == '50-54', 52,
-                                                                  ifelse(df$AgeCategory == '55-59', 57,
-                                                                         ifelse(df$AgeCategory == '60-64', 62,
-                                                                                ifelse(df$AgeCategory == '65-69', 67,
-                                                                                       ifelse(df$AgeCategory == '70-74', 72,
-                                                                                              ifelse(df$AgeCategory == '75-79', 77,
-                                                                                                     ifelse(df$AgeCategory == '80 or older', 80, 0)))))))))))))
+df$AgeCategory = ifelse(df$AgeCategory == '18-24', 'Young',
+                        ifelse(df$AgeCategory == '25-29', 'Young',
+                               ifelse(df$AgeCategory == '30-34', 'Young',
+                                      ifelse(df$AgeCategory == '35-39', 'Young',
+                                             ifelse(df$AgeCategory == '40-44', 'Young',
+                                                    ifelse(df$AgeCategory == '45-49', 'Old',
+                                                           ifelse(df$AgeCategory == '50-54', 'Old',
+                                                                  ifelse(df$AgeCategory == '55-59', 'Old',
+                                                                         ifelse(df$AgeCategory == '60-64', 'Old',
+                                                                                ifelse(df$AgeCategory == '65-69', 'Old',
+                                                                                       ifelse(df$AgeCategory == '70-74', 'Old',
+                                                                                              ifelse(df$AgeCategory == '75-79', 'Old',
+                                                                                                     ifelse(df$AgeCategory == '80 or older', 'Old', 0)))))))))))))
 
+df$AgeCategory = factor(df$AgeCategory)
 set.seed(100)
 n = nrow(df)
 train = sample(1:nrow(df),nrow(df)/2, replace=FALSE)
@@ -50,16 +50,16 @@ plot(performance(ROCRpred,'tnr','fnr'),colorize=TRUE,
      print.cutoffs.at=seq(0,1,by=0.05), text.adj=c(-0.2,1.7))
 auc_ROCR <- performance(ROCRpred, measure = "auc")
 auc_ROCR@y.values[[1]] 
-12199 / (12199 + 1479)
-1239 / (1239+144981)
+12396 / (12396 + 1282)
+1022 / (1022+145198)
 
 glm.prob = predict(glm.fit, df[test,], type='response') 
 glm.pred = rep(0,dim(df[test,])[1])
 glm.pred[glm.prob > 0.1] = 1
 table(glm.pred, df[test,]$HeartDisease)
 1-mean(glm.pred == df[test,]$HeartDisease)
-3451 / (3451 + 10227)
-32165 / (32165+114055)
+3918 / (3918 + 9760)
+33355 / (33355+112865)
 
 
 library(corrplot)
